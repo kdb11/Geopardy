01153fb08cd8afd40081d0d12b8834ee5dcd2ac2
kdb11 <kevin.badwi@medieinstitutet.se>
2023-01-07 01:21:06 +0100

    get category title + category id

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-function getApi () {
-fetch('https://jservice.io//api/clues')
-  .then((response) => response.json())
-  .then((data) => console.log(data)); }
 
-  setTimeout(() => {
-    getApi();
-}, 1000);
+let apiData;
+let apiDataId = [];
+let i = 0;
+
+function getApi () {
+  fetch('https://jservice.io/api/clues')
+  .then((response) => response.json())
+  /* .then((data) => JSON.stringify(data)) */
+  .then((data) => apiData = data) 
+  /* .then((data) => apiDataId = data[i].answer); */
+}
+
+  getApi();
+
+  setTimeout((data) => {
+    /* console.log(data[i].category.title);
+    JSON.parse(apiData) */
+    for (let i = 0; i < 100; i++) {
+      if (apiData[i].category.clues_count > 10 ) {
+        apiDataId.push((apiData[i].category.id),(apiData[i].category.title));
+        console.log(apiDataId)
+      }
+      
+      /* apiDataId = apiData[i].category.title;
+      console.log(apiDataId) */
+    }
+    console.log(apiData)
+    /* console.log(apiDataId) */
+}, 2000);
+
+
