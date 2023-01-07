a1f4433d05f32a71d7eadc80ed02fdc81813220f
kdb11 <kevin.badwi@medieinstitutet.se>
2023-01-07 19:56:52 +0100

    fetch clues depending on category

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ let categoryTitel5 = document.getElementById('categoryTitel5');
 
       /* setTimeout (categoryTitel1 = newCategory[i].category, 5000); */
     }
-    console.log(apiData)
+    /* console.log(apiData) */
 
 
 categoryTitel1.innerHTML = newCategory[0].category;
@@ -35,3 +35,29 @@ categoryTitel2.innerHTML = newCategory[1].category;
 categoryTitel3.innerHTML = newCategory[2].category;
 categoryTitel4.innerHTML = newCategory[3].category;
 categoryTitel5.innerHTML = newCategory[4].category;
+
+
+
+
+        fetch(`https://jservice.io/api/category?id=${newCategory[0].id}`)
+           .then(response => response.json())
+           .then(data => {console.log(data.clues[0]);
+           });
+        fetch(`https://jservice.io/api/category?id=${newCategory[1].id}`)
+           .then(response => response.json())
+           .then(data => {console.log(data);
+           });
+        fetch(`https://jservice.io/api/category?id=${newCategory[2].id}`)
+           .then(response => response.json())
+           .then(data => {console.log(data);
+           });
+        fetch(`https://jservice.io/api/category?id=${newCategory[3].id}`)
+           .then(response => response.json())
+           .then(data => {console.log(data);
+           });
+        fetch(`https://jservice.io/api/category?id=${newCategory[4].id}`)
+           .then(response => response.json())
+           .then(data => {console.log(data);
+           });
+
+
