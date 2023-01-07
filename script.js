97b27f22c77ae077b87e5d1857788bd254c3cd14
kdb11 <kevin.badwi@medieinstitutet.se>
2023-01-07 18:16:14 +0100

    removed settimout and made async

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 
 let apiData;
-/* let apiDataId = []; */
 let apiDataId = [];
-let i = 0;
 
-function getApi () {
-  fetch('https://jservice.io/api/clues')
+
+  await fetch('https://jservice.io/api/clues')
   .then((response) => response.json())
-  /* .then((data) => JSON.stringify(data)) */
   .then((data) => apiData = data) 
-  /* .then((data) => apiDataId = data[i].answer); */
-}
 
-  getApi();
-
-  setTimeout(() => {
-    /* console.log(data[i].category.title);
-    JSON.parse(apiData) */
     for (let i = 0; i < 100; i++) {
       if (apiData[i].category.clues_count > 9) {
-        /* apiDataId.push((apiData[i].category.id),(apiData[i].category.title)); */
         apiDataId.push({id: apiData[i].category.id , category: apiData[i].category.title});
       }
       
@@ -29,11 +18,9 @@ function getApi () {
         })
       })
       console.log(newCategory)
-      /* apiDataId = apiData[i].category.title;
-      console.log(apiDataId) */
     }
     console.log(apiData)
-    /* console.log(apiDataId) */
-}, 1500);
+
+
 
 
